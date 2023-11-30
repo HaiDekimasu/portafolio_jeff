f071ef9cb462dabb7cf2a5874bd5043f984b1cab
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-29 23:07:01 -0300

    agregando visualizacion al models imgpresent

--- Portafolio/models.py
+++ Portafolio/models.py
@@ -26,3 +26,6 @@ class Imgpresent(models.Model):
     imagen = models.ImageField(upload_to='Portafolio/images')
     title = models.CharField( max_length=100)
     id = models.AutoField(primary_key=True)
+    
+    def __str__(self)-> str:
+        return self.imagen
