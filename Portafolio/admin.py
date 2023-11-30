0f9da1021af01a405f7f2e503a74ed1d94749162
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-29 22:47:38 -0300

    registro de models imgpresent

--- Portafolio/admin.py
+++ Portafolio/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Projecto, Presentacion
+from .models import Projecto, Presentacion,Imgpresent
 
 # Register your models here.
 
 admin.site.register(Projecto),
-admin.site.register(Presentacion)
+admin.site.register(Presentacion),
+admin.site.register(Imgpresent)
