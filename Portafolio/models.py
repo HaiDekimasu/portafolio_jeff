f947158dc9b676b2c80e65c3e4222f65671370b1
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-30 11:12:45 -0300

    modificacion visualizacion models Imgpresent

--- Portafolio/models.py
+++ Portafolio/models.py
@@ -28,5 +28,5 @@ class Imgpresent(models.Model):
     id = models.AutoField(primary_key=True)
     
     
-    def __str__(self)-> str:
-        return self.title
+    def __str__(self) -> str:
+        return self.imagen
