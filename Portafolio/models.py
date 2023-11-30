43c7d99928cd98738f84d13c05e6dadbff2f553c
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-29 22:45:13 -0300

    modificacion de models 0016_alter_imgpresent_imagen

--- Portafolio/models.py
+++ Portafolio/models.py
@@ -23,6 +23,6 @@ class Presentacion(models.Model):
     
     
 class Imgpresent(models.Model):
-    imagen = models.ImageField(upload_to='media/Portafolio/images')
+    imagen = models.ImageField(upload_to='Portafolio/images')
     title = models.CharField( max_length=100)
     id = models.AutoField(primary_key=True)
