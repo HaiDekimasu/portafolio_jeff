b85ca1d583af4d79d9fe7fc8b670a9be9260e6aa
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-30 17:02:06 -0300

    prueba revision for

--- portafolio_jeff/views.py
+++ portafolio_jeff/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 from Portafolio.models import Projecto, Presentacion, Imgpresent
-from django.core.exceptions import FieldDoesNotExist
 
 def home(request, Projecto_id=None):
     projectos = Projecto.objects.all()
     presentaciones = Presentacion.objects.all()
-    return render(request, 'Principal.html', {'projectos': projectos, 'presentaciones': presentaciones})
-
-def home(request):
-    # Obtener las imágenes
     imgs = Imgpresent.objects.all()
+    return render(request, 'Principal.html', {'projectos': projectos, 'presentaciones': presentaciones,'imgs': imgs})
 
-    return render(request, 'Principal.html', {'imgs': imgs})
