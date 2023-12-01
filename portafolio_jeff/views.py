9fb70aa14f97359dce533c297b7b869b69c0c4a0
Haidekimasu <cadevillajefferson@gmail.com>
2023-11-30 22:28:52 -0300

    prueba autoplay 01

--- portafolio_jeff/views.py
+++ portafolio_jeff/views.py
@@ -4,6 +4,7 @@ from Portafolio.models import Projecto, Presentacion, Imgpresent
 def home(request, Projecto_id=None):
     projectos = Projecto.objects.all()
     presentaciones = Presentacion.objects.all()
-    imgs = Imgpresent.objects.all()
+    imgs = Imgpresent.objects.filter(autoplay=True)
+
     return render(request, 'Principal.html', {'projectos': projectos, 'presentaciones': presentaciones,'imgs': imgs})
 
