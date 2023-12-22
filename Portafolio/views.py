73af522a5ca9601ec14f441532419045b15e8b0a
Haidekimasu <cadevillajefferson@gmail.com>
2023-12-22 17:18:40 -0300

    modified:   Portafolio/views.py

--- Portafolio/views.py
+++ Portafolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Projecto, Presentacion,Imgpresent, Contacto
-from Portafolio.forms import Contactoform
+from .models import Projecto, Presentacion
+from .forms import Contactoform
 
 # Create your views here.
 def vporfolio(request):
@@ -10,12 +10,13 @@ def vporfolio(request):
 
 
 def contacto(request):
-    data = {'form': Contactoform()}
-    if request.method == 'POST':
-        formulario = Contactoform(data=request.POST)
-        if request.method.is_valid():
-            formulario.save()
-            data['mensaje'] = "Contacto Guardado"  
-        else:
-            data['form'] = formulario
-    return render(request, './partials/contacto.html',data)
+    # data = {'form': Contactoform()}
+    # if request.method == 'POST':
+    #     formulario = Contactoform(data=request.POST)
+    #     if request.method.is_valid():
+    #         formulario.save()
+    #         data['mensaje'] = "Contacto Guardado"  
+    #     else:
+    #         data['form'] = formulario
+    return render(request, './contacto.html',{})
+    
