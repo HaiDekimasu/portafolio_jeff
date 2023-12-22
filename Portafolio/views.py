b8ae7d9f1616cd399b0d9991214cf324a1238295
Haidekimasu <cadevillajefferson@gmail.com>
2023-12-22 18:00:41 -0300

    modified:   Portafolio/views.py

--- Portafolio/views.py
+++ Portafolio/views.py
@@ -10,13 +10,13 @@ def vporfolio(request):
 
 
 def contacto(request):
-    # data = {'form': Contactoform()}
-    # if request.method == 'POST':
-    #     formulario = Contactoform(data=request.POST)
-    #     if request.method.is_valid():
-    #         formulario.save()
-    #         data['mensaje'] = "Contacto Guardado"  
-    #     else:
-    #         data['form'] = formulario
-    return render(request, './contacto.html',{})
+    data = {'form': Contactoform()}
+    if request.method == 'POST':
+        formulario = Contactoform(data=request.POST)
+        if request.method.is_valid():
+            formulario.save()
+            data['mensaje'] = "Contacto Guardado"  
+        else:
+            data['form'] = formulario
+    return render(request, 'portafolio/contacto.html',data)
     
