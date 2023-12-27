9ea57b6c1fbebadbc2bdca28cae4a108488eed73
Haidekimasu <cadevillajefferson@gmail.com>
2023-12-26 23:36:59 -0300

    modified:   Portafolio/__pycache__/views.cpython-311.pyc 	modified:   Templates/portafolio/contacto.html 	modified:   portafolio_jeff/__pycache__/settings.cpython-311.pyc 	modified:   portafolio_jeff/__pycache__/urls.cpython-311.pyc 	modified:   portafolio_jeff/settings.py 	modified:   portafolio_jeff/urls.py

--- portafolio_jeff/settings.py
+++ portafolio_jeff/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Portafolio',
     'blog',
+    'crispy_forms',
 ]
+CRISPY_TEMPLATE_PACK= "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
