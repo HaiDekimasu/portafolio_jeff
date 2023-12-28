379524f9cafe481ad6211d71584ce9119a116919
Haidekimasu <cadevillajefferson@gmail.com>
2023-12-27 23:27:36 -0300

    modified:   Portafolio/__pycache__/forms.cpython-311.pyc 	modified:   Portafolio/forms.py 	modified:   static/styles/estilos.css

--- Portafolio/forms.py
+++ Portafolio/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Contacto
 
 class Contactoform(forms.ModelForm):
+    nombre = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    correo = forms.EmailField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={"class":"form-control"}))
     class Meta:
         model = Contacto
         fields = '__all__'
