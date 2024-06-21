e6484c767b6b10d635d2086eff7611ead2ed00bc
Haidekimasu <cadevillajefferson@gmail.com>
2024-06-21 15:16:31 -0400

    revision 21/6

--- portafolio_jeff/settings.py
+++ portafolio_jeff/settings.py
@@ -118,13 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Replace BASE_DIR with your project's base directory
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Replace BASE_DIR with your project's base directory
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
